574955cc445cb03d7556ef16d8e6a1a05acf66a5
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2023-04-05 02:13:05 +0300

    add fixtures

--- conftest.py
+++ conftest.py
@@ -1,6 +1,28 @@
 import pytest
+from selenium import webdriver
+
+DRIVERS = {
+    'chrome': '/drivers/chromedriver',
+    'firefox': '/drivers/geckodriver'
+}
 
 
-@pytest.fixture()
+def pytest_addoption(parser):
+    parser.addoption("--driver", default="chrome")
+
+
+@pytest.fixture
+def browser_instance(request):
+    browser_name = request.config.getoption("--driver")
+    if browser_name == 'chrome':
+        browser = webdriver.Chrome(DRIVERS['chrome'])
+    elif browser_name == 'firefox':
+        browser = webdriver.Firefox(executable_path=DRIVERS['firefox'])
+    browser.start_client()
+    request.addfinalizer(browser.close)
+    return browser
+
+
+@pytest.fixture
 def some_fixture():
     return 2
